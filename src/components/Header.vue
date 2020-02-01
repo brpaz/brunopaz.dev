d90f369c7a2850a6dbec69e992ec09785a275a23
Bruno Paz <brunopaz@sapo.pt>
2020-02-01 16:17:12 +0000

    feat: add uses page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,43 +1,108 @@
 <template>
-   <header class="bg-white sticky top-0 mb-8 w-full z-50">
-      <nav class="container mx-auto flex flex-wrap justify-between items-center py-4">
-        <div>
-          <a href="/" class="no-underline font-sans uppercase text-primary text-3xl text-copy-primary hover:text-copy-primary">Bruno Paz</a>
-        </div>
-        <div class="block lg:hidden">
-          <button title="Toggle Menu" @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600 outline-none">
-            <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
-          </button>
-        </div>
-        <ul
-          class="uppercase tracking-wide font-bold w-full block flex-grow lg:flex lg:flex-initial lg:w-auto items-center mt-8 lg:mt-0"
-          :class="isOpen ? 'block': 'hidden'"
+  <header class="bg-white sticky top-0 mb-8 w-full z-50">
+    <nav
+      class="container mx-auto flex flex-wrap justify-between items-center py-4"
+    >
+      <div>
+        <a
+          href="/"
+          class="no-underline font-sans uppercase text-primary text-3xl text-copy-primary hover:text-copy-primary"
+          >Bruno Paz</a
         >
-          <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
-            <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-copy-primary hover:text-gray-600">About</a>
-            <g-link v-else to="/#about" class="text-copy-primary hover:text-gray-600">About</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
-            <a v-if="$route.path === '/'" href="/#whatido" v-scroll-to="'#whatido'" class="text-copy-primary hover:text-gray-600">What I do?</a>
-            <g-link v-else to="/#contact" class="text-copy-primary hover:text-gray-600">What I do?</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
-            <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-copy-primary hover:text-gray-600">Projects</a>
-            <g-link v-else to="/#projects" class="text-copy-primary hover:text-gray-600">Projects</g-link>
-          </li>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          title="Toggle Menu"
+          @click="toggle"
+          class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600 outline-none"
+        >
+          <svg
+            class="current-color h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" />
+          </svg>
+        </button>
+      </div>
+      <ul
+        class="uppercase tracking-wide font-bold w-full block flex-grow lg:flex lg:flex-initial lg:w-auto items-center mt-8 lg:mt-0"
+        :class="isOpen ? 'block' : 'hidden'"
+      >
+        <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
+          <a
+            v-if="$route.path === '/'"
+            href="/#about"
+            v-scroll-to="'#about'"
+            class="text-copy-primary hover:text-gray-600"
+            >About</a
+          >
+          <g-link
+            v-else
+            to="/#about"
+            class="text-copy-primary hover:text-gray-600"
+            >About</g-link
+          >
+        </li>
+        <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
+          <a
+            v-if="$route.path === '/'"
+            href="/#whatido"
+            v-scroll-to="'#whatido'"
+            class="text-copy-primary hover:text-gray-600"
+            >What I do?</a
+          >
+          <g-link
+            v-else
+            to="/#contact"
+            class="text-copy-primary hover:text-gray-600"
+            >What I do?</g-link
+          >
+        </li>
+        <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
+          <a
+            v-if="$route.path === '/'"
+            href="/#projects"
+            v-scroll-to="'#projects'"
+            class="text-copy-primary hover:text-gray-600"
+            >Projects</a
+          >
+          <g-link
+            v-else
+            to="/#projects"
+            class="text-copy-primary hover:text-gray-600"
+            >Projects</g-link
+          >
+        </li>
 
-          <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
-            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600">Contact</a>
-            <g-link v-else to="/#contact" class="text-copy-primary hover:text-gray-600">Contact</g-link>
-          </li>
-          <li>
-            <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-
+        <li class="mr-8 mb-6 lg:mb-0" @click="onMenuItemClick">
+          <a
+            v-if="$route.path === '/'"
+            href="/#contact"
+            v-scroll-to="'#contact'"
+            class="text-copy-primary hover:text-gray-600"
+            >Contact</a
+          >
+          <g-link
+            v-else
+            to="/#contact"
+            class="text-copy-primary hover:text-gray-600"
+            >Contact</g-link
+          >
+        </li>
+        <li class="mr-8 mb-6 lg:mb-0">
+          <g-link to="/blog" class="text-copy-primary hover:text-gray-600"
+            >Blog</g-link
+          >
+        </li>
+        <li>
+          <g-link to="/uses" class="text-copy-primary hover:text-gray-600"
+            >Uses</g-link
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -46,15 +111,15 @@ export default {
   data() {
     return {
       isOpen: false
-    }
+    };
   },
   methods: {
     toggle() {
-      this.isOpen = !this.isOpen
+      this.isOpen = !this.isOpen;
     },
     onMenuItemClick() {
-      this.toggle()
+      this.toggle();
     }
   }
-}
+};
 </script>
---
+++ src/pages/Uses.vue
@@ -0,0 +1,47 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2>Uses</h2>
+
+      <p class="mb-8">
+        This page is inspired by
+        <a href="https://wesbos.com/" target="_blank" rel="noopener nofollow"
+          >Wes Bos</a
+        >&nbsp;
+        <a href="https://uses.tech" target="_blank" rel="noopener nofollow"
+          >uses pages</a
+        >
+        and contains my detailed setup and tools that I use for Development.
+      </p>
+
+      <div v-for="section in uses" :key="section.id" class="mb-8">
+        <h3>{{ section.title }}</h3>
+
+        <p class="mb-4">{{ section.description }}</p>
+
+        <ul class="list-disc pl-8">
+          <li
+            v-for="sectionItem in section.items"
+            :key="sectionItem.name"
+            class="pb-4"
+          >
+            <a :href="sectionItem.url">{{ sectionItem.name }}</a> -
+            {{ sectionItem.description }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import uses from '~/data/uses.yml';
+export default {
+  components: {},
+  data() {
+    return {
+      uses
+    };
+  }
+};
+</script>
