c896c6de22c8b827a0c28ebcb1f42c7d7651bdf3
Bruno Paz <brunopaz@sapo.pt>
2020-06-11 21:20:33 +0100

    feat: Add web monetization to Blog

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -8,21 +8,30 @@
           :to="tag.path"
           v-for="tag in $page.post.tags"
           :key="tag.id"
-          class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-green-300">
+          class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-green-300"
+        >
           {{ tag.title }}
         </g-link>
       </div>
       <div class="markdown-body mb-8" v-html="$page.post.content" />
-      
 
-      <div v-if="$page.post.devto_url" class="mb-8 bg-gray-300 border-t border-b border-gray-500 text-gray-500 px-4 py-3">
-        <p class="font-bold">This post was cross posted on <a :href="$page.post.devto_url">dev.to</a></p>
+      <div
+        v-if="$page.post.devto_url"
+        class="mb-8 bg-gray-300 border-t border-b border-gray-500 text-gray-500 px-4 py-3"
+      >
+        <p class="font-bold">
+          This post was cross posted on
+          <a :href="$page.post.devto_url">dev.to</a>
+        </p>
       </div>
       <div class="mb-8 flex justify-end">
-          <div class="addthis_inline_share_toolbox"></div>
+        <div class="addthis_inline_share_toolbox"></div>
       </div>
       <div class="mb-8">
-          <vue-disqus :shortname="disqusShortname" :identifier="$page.post.title"></vue-disqus>
+        <vue-disqus
+          :shortname="disqusShortname"
+          :identifier="$page.post.title"
+        ></vue-disqus>
       </div>
       <div class="mb-8">
         <g-link to="/blog" class="font-bold uppercase">Back to Blog</g-link>
@@ -57,26 +66,30 @@ export default {
     return {
       title: pageTitle,
       meta: [
-         { name: "description", content: this.$page.post.summary },
-         { name: "twitter:creator", content: meta.twitter_handler },
-         { name: "og:title", content: this.$page.post.title },
-
-        { "twitter:creator": "@brunopaz88" },
+        { name: 'description', content: this.$page.post.summary },
+        { name: 'twitter:creator', content: meta.twitter_handler },
+        { name: 'og:title', content: this.$page.post.title },
+        { name: 'monetization', content: meta.monetization_pointer_url },
+        { 'twitter:creator': '@brunopaz88' },
         { 'og:title': pageTitle },
-        { 'og:description': this.$page.post.summary}
+        { 'og:description': this.$page.post.summary }
       ],
       script: [
-        { "src": "https://s7.addthis.com/js/300/addthis_widget.js#pubid=" + process.env.GRIDSOME_ADDTHIS_ID,  body: true}
+        {
+          src:
+            'https://s7.addthis.com/js/300/addthis_widget.js#pubid=' +
+            process.env.GRIDSOME_ADDTHIS_ID,
+          body: true
+        }
       ]
-    }
+    };
   },
   data() {
     return {
       disqusShortname: process.env.GRIDSOME_DISQUS_SHORTNAME
-    }
+    };
   }
-}
+};
 </script>
 
 <style src="~/assets/css/github-markdown.css" />
-
