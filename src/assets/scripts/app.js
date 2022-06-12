a68edcd2d9f8a716535531661a63d1f6dc86d61b
Bruno Paz <184563+brpaz@users.noreply.github.com>
2022-06-12 16:43:27 +0100

    fix(projects): image gallery not loading on page navigation plus some layout shift improvements

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -2,11 +2,13 @@ import Swup from "swup";
 import SwupHeadPlugin from "@swup/head-plugin";
 import SwupSlideTheme from "@swup/slide-theme";
 import SwupA11yPlugin from "@swup/a11y-plugin";
+import SwupScriptsPlugin from "@swup/scripts-plugin";
 
 const swup = new Swup({
   animateHistoryBrowsing: true,
   plugins: [
     new SwupHeadPlugin(),
+    new SwupScriptsPlugin({ body: false }),
     new SwupA11yPlugin(),
     new SwupSlideTheme({
       reversed: true,
