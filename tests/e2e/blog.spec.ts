5055d704e1bca4aedf27cdaa779514eb6eda70c8
Bruno Paz <184563+brpaz@users.noreply.github.com>
2026-01-18 11:19:43 +0000

    chore: fix tests

--- tests/e2e/blog.spec.ts
+++ tests/e2e/blog.spec.ts
@@ -6,32 +6,25 @@ test.describe('Blog Page', () => {
   });
 
   test('Renders page', async ({ page }) => {
-    // Check if the <h1> contains the text "My Blog"
     const header = page.locator('h1');
     await expect(header).toHaveText('My Blog');
 
-    // Check if the link with href containing '/blog' has the class 'active'
     const navLink = page.locator('.main-nav a[href*="/blog/page/1"]');
     await expect(navLink).toHaveClass(/active/);
 
-    // Check if the current page indicator in the pagination is "1"
-    const currentPage = page.locator('.pagination a[class*="current-page"]');
+    const currentPage = page.locator('.current-page');
     await expect(currentPage).toHaveText('1');
   });
 
   test('Paginates Blog posts', async ({ page }) => {
-    // Click on the second pagination link
-    const paginationLinks = page.locator('.pagination a');
-    await paginationLinks.nth(1).click();
+    const nextPageLink = page.locator('a[href="/blog/page/2"]');
+    await nextPageLink.click();
 
-    // Wait for the pagination to update
-    await page.waitForTimeout(1000);
+    await page.waitForLoadState('networkidle');
 
-    // Check if the current page indicator in the pagination is "2"
-    const currentPage = page.locator('.pagination a[class*="current-page"]');
+    const currentPage = page.locator('.current-page');
     await expect(currentPage).toHaveText('2');
 
-    // Check if the URL includes "/blog/page/2"
     await expect(page).toHaveURL(/\/blog\/page\/2$/);
   });
 });
