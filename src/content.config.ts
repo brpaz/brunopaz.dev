cea43867496a0241e7f8adbfeeffef6e8ced5ae0
Bruno Paz <184563+brpaz@users.noreply.github.com>
2026-06-21 16:04:46 +0100

    feat: design improvements. replace projects page with full CV

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,5 @@
 import { blogPostsCollection } from './content/posts';
-import { projectsCollection } from './content/projects';
 
 export const collections = {
-  projects: projectsCollection,
   blogPosts: blogPostsCollection,
 };
