13dd74c128bfc3f5fd5e5eb60e7b3735cb514436
Bruno Paz <184563+brpaz@users.noreply.github.com>
2026-06-21 17:35:04 +0100

    feat: replace content with collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,9 @@
 import { blogPostsCollection } from './content/posts';
+import { experiencesCollection } from './content/experiences';
+import { projectsCollection } from './content/projects';
 
 export const collections = {
   blogPosts: blogPostsCollection,
+  experiences: experiencesCollection,
+  projects: projectsCollection,
 };
--- src/content/experiences.ts
+++ src/content/experiences.ts
@@ -2,12 +2,14 @@ import { defineCollection } from 'astro:content';
 import { file } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-export const projectsCollection = defineCollection({
-  loader: file('./src/content/projects.json'),
+export const experiencesCollection = defineCollection({
+  loader: file('./src/content/experience.json'),
   schema: z.object({
-    order: z.number(),
-    name: z.string(),
+    company: z.string(),
+    position: z.string(),
     url: z.string().optional(),
+    startDate: z.string(),
+    endDate: z.string().optional(),
     technologies: z.array(z.string()).optional(),
     highlights: z.array(z.string()),
   }),
